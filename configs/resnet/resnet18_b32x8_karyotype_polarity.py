938db5317bb3bd371d21a37e657b9af747ce5a6d
liyinhao <liyinhao0413@qq.com>
2021-03-12 11:13:54 +0800

    Add test dataset

--- configs/resnet/resnet18_b32x8_karyotype_polarity.py
+++ configs/resnet/resnet18_b32x8_karyotype_polarity.py
@@ -1,4 +1,5 @@
 _base_ = [
-    '../_base_/models/resnet18.py', '../_base_/datasets/karyotype_polarity_bs32.py',
+    '../_base_/models/resnet18.py',
+    '../_base_/datasets/karyotype_polarity_bs32.py',
     '../_base_/schedules/imagenet_bs256.py', '../_base_/default_runtime.py'
 ]
--- tools/generate_ann_file.py
+++ tools/generate_ann_file.py
@@ -1,11 +1,17 @@
-import os
-import numpy as np
 import argparse
+import os
+
+import numpy as np
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate ann file')
-    parser.add_argument('--data-root', default='./data/karyotype_polarity', help='Root path of data')
-    parser.add_argument('--train-ratio', type=float, default=0.7)
+    parser.add_argument(
+        '--data-root',
+        default='./data/karyotype_polarity',
+        help='Root path of data')
+    parser.add_argument('--train-ratio', type=float, default=0.6)
+    parser.add_argument('--valid-ratio', type=float, default=0.2)
 
     args = parser.parse_args()
     return args
@@ -20,7 +26,11 @@ def main():
         annos += [[os.path.join(folder, name), i] for name in file_names]
     np.random.shuffle(annos)
     train_annos = annos[:int(len(annos) * args.train_ratio)]
-    valid_annos = annos[int(len(annos) * args.train_ratio):]
+    valid_annos = annos[int(len(annos) * args.train_ratio
+                            ):int(len(annos) * args.train_ratio) +
+                        int(len(annos) * args.valid_ratio)]
+    test_annos = annos[int(len(annos) * args.train_ratio) +
+                       int(len(annos) * args.valid_ratio):]
     with open(os.path.join(args.data_root, 'train.txt'), 'w') as f:
         for path, label in train_annos:
             f.write(f'{path} {label}\n')
@@ -28,6 +38,11 @@ def main():
     with open(os.path.join(args.data_root, 'valid.txt'), 'w') as f:
         for path, label in valid_annos:
             f.write(f'{path} {label}\n')
+
+    with open(os.path.join(args.data_root, 'test.txt'), 'w') as f:
+        for path, label in test_annos:
+            f.write(f'{path} {label}\n')
+
 
 if __name__ == '__main__':
     main()
