462f2be49bf1e8133269c49edf74ec1bd9e31b54
Yinhao Li <liyinhao20@mails.ucas.edu.cn>
2021-05-24 02:17:49 -0400

    Able to train gleason_2019.

---
+++ configs/_base_/datasets/gleason_2019_bs32.py
@@ -0,0 +1,45 @@
+# dataset settings
+dataset_type = 'ImageNet'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='RandomResizedCrop', size=1024),
+    dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='ToTensor', keys=['gt_label']),
+    dict(type='Collect', keys=['img', 'gt_label'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', size=(1024, -1)),
+    dict(type='CenterCrop', crop_size=1024),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='ImageToTensor', keys=['img']),
+    dict(type='Collect', keys=['img'])
+]
+data = dict(
+    samples_per_gpu=32,
+    workers_per_gpu=10,
+    train=dict(
+        type=dataset_type,
+        data_prefix='data/gleason_2019',
+        classes=[0, 1, 2, 3],
+        ann_file='data/gleason_2019/train.txt',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        data_prefix='data/gleason_2019',
+        classes=[0, 1, 2, 3],
+        ann_file='data/gleason_2019/valid.txt',
+        pipeline=test_pipeline),
+    test=dict(
+        # replace `data/val` with `data/test` for standard test
+        type=dataset_type,
+        data_prefix='data/gleason_2019',
+        classes=[0, 1, 2, 3],
+        ann_file='data/gleason_2019/test.txt',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='accuracy')
---
+++ tools/gen_labels.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+
+import mmcv
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a model')
+    parser.add_argument('--ann-dir', help='segmentation annotation path')
+    parser.add_argument('--img-dir', help='image path')
+    parser.add_argument(
+        '--tar-path', help='target path for classification path')
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    args = parse_args()
+    img_dir = args.img_dir
+    ann_dir = args.ann_dir
+    tar_path = args.tar_path
+
+    names = os.listdir(ann_dir)
+    anns = []
+
+    for i, name in enumerate(names):
+        ann = mmcv.imread(os.path.join(ann_dir, name), 0)
+        anns.append(ann.max())
+        print(f'{name} finished')
+
+    with open(os.path.join(tar_path), 'w') as f:
+        for ann in anns:
+            f.write(f'{os.path.join(img_dir, name)} {ann}\n')
+
+
+if __name__ == '__main__':
+    main()
