3eca7a8b771f14256044a7f999a7b9bfd5a6d6b8
liyinhao <liyinhao0413@qq.com>
2022-01-17 14:29:34 +0800

    Add resnet152 configs.

---
+++ configs/resnet/resnet152_8xb16_glomerulus_M.py
@@ -0,0 +1,22 @@
+_base_ = [
+    '../_base_/models/resnet152_cifar.py',
+    '../_base_/datasets/glomerulus_bs16_M.py',
+    '../_base_/schedules/cifar10_bs128.py', '../_base_/default_runtime.py'
+]
+
+# model settings
+model = dict(
+    type='ImageClassifier',
+    backbone=dict(
+        type='ResNet_CIFAR',
+        depth=152,
+        num_stages=4,
+        out_indices=(3, ),
+        style='pytorch'),
+    neck=dict(type='GlobalAveragePooling'),
+    head=dict(
+        type='LinearClsHead',
+        num_classes=2,
+        in_channels=2048,
+        loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
+    ))
